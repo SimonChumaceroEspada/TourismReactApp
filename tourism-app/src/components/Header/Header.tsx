2e81f713d9004ae9b6be99bd847072e126960959
Simon <simonchumacero26@gmail.com>
2024-09-01 12:37:29 -0400

    refactor: Remove unnecessary buttons and update Header component

--- tourism-app/src/components/Header/Header.tsx
+++ tourism-app/src/components/Header/Header.tsx
@@ -47,31 +47,7 @@ const Header: React.FC<HeaderProps> = ({ onLanguageChange }) => {
               </li>
             ))}
           </ul>
-
-          <div className="mt-4 md:mt-0 relative flex-grow flex justify-center order-2 md:order-none">
-            <input
-              type="search"
-              className="bg-[#EEEEEE] rounded-lg py-2 pl-10 pr-4 focus:outline-none w-full max-w-sm"
-              placeholder="Search..."
-            />
-            <span className="absolute inset-y-0 left-3 flex items-center pl-3 pointer-events-none">
-              <svg
-                className="h-5 w-5 text-[#B65172]"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.537 4.536a1 1 0 01-1.414 1.414l-4.536-4.537A6 6 0 012 8z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </span>
-          </div>
-
           <div className="mt-4 md:mt-0 flex justify-center md:justify-end space-x-4">
-            <button className="bg-[#FFE051] text-white w-8 h-8 flex items-center justify-center rounded-full hover:bg-[#F5E300]">
-            </button>
             <button
               onClick={onLanguageChange}
               className="bg-[#209674] text-white w-8 h-8 flex items-center justify-center rounded-full hover:bg-[#1E8E7E]"
