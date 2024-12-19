45bbb6d9de083423472d4061a9371c87413917c3
Simon <simonchumacero26@gmail.com>
2024-12-19 18:47:45 -0400

    refactor: Enhance Card component layout with improved styling; adjust image dimensions and container properties for better visual presentation

--- tourism-app/src/components/Card/Card.tsx
+++ tourism-app/src/components/Card/Card.tsx
@@ -11,15 +11,17 @@ const Card: React.FC<CardProps> = ({ name, description, imageSrc }) => {
   const { language } = useLanguage();
 
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+    <div className="w-[600px] rounded-2xl overflow-hidden shadow-lg bg-white">
       <h1 className="text-3xl font-bold text-center py-4 text-[#B65172]">
         {language === "en" ? `Welcome to ${name}` : `Bienvenidos a ${name}`}
       </h1>
-      <img
-        className="w-full h-64 object-cover"
-        src={imageSrc}
-        alt={name}
-      />
+      <div className="px-4">
+        <img
+          className="w-full h-[400px] object-cover rounded-2xl"
+          src={imageSrc}
+          alt={name}
+        />
+      </div>
       <div className="px-6 py-4">
         <p className="text-gray-700 text-base">
           {language === "en" ? description : description}
